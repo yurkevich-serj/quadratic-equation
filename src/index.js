f530a7a5a31c5d07738ed3abe6052a09b5624ea9
yurkevich-serj <yurkevichserj@gmail.com>
2018-02-21 19:14:54 +0300

    All roots found

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+'use strict'
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let array = equation.split(' ');
+  let a = array[0];
+  let b = array.slice(3,5).join('');
+  let c = array.slice(7,10).join('');
+  let discriminant= b*b - 4*a*c;
+  let x1 = (-b + Math.sqrt(discriminant))/2/a;
+  let x2 = (-b - Math.sqrt(discriminant))/2/a;
+  
+  return [Math.round(Math.min(x1,x2)),Math.round(Math.max(x1,x2))];
+
+
+
 }
